d259b98426e2d707baa12f49054e3113f1655284
HanshuZhou <342136474@qq.com>
2018-12-13 23:48:37 -0700

    1

---
+++ zhs/generator.js
@@ -0,0 +1,77 @@
+// Code that generates the random lorem ipsum text
+
+// Create a new object called loremIpsum by invoking the GenerateNewText constructor function
+const loremIpsum = new GenerateNewText();
+
+// Constructor function that creates an object with the sentences property
+function GenerateNewText() {
+  // Add property to the object
+  this.sentences =
+    [
+"Baby One More Time",
+"(You Drive Me) Crazy",
+"Sometimes",
+"Soda Pop",
+"Born To Make You Happy",
+"From The Bottom Of My Broken Heart",
+"I Will Be There",
+"I Will Still Love You",
+"Thinkin About You",
+"Email My Heart",
+"Beat Goes On",
+"Oops! ...I Did It Again",
+"Stronger",
+"Don't Go Knocking On My Door",
+"Satisfaction",
+"Don't Let Me Be The Last To Know",
+"What U See (Is What You Get)",
+"Lucky",
+"One Kiss From You",
+"Where Are You Now",
+"Can't Make You Love Me",
+"When Your Eyes Say It",
+"Dear Diary",
+"Girl In The Mirror",
+"You Got It All",
+   ];
+}
+
+// Method to the GenerateNewText constructor function that generates a random sentence
+GenerateNewText.prototype.getRandomSentence = function() {
+  let randomSentence = this.sentences[Math.floor(Math.random() * this.sentences.length)]
+	return randomSentence;
+}
+
+// Method to the GenerateNewText constructor function that generates a paragraph from random sentences
+GenerateNewText.prototype.getParagraph = function() {
+  let paragraph = "";
+  // Set the minimum number of words
+  let minimumCharacterLength = 250;
+  let firstSentence = true;
+  while (paragraph.length < minimumCharacterLength) {
+    if (firstSentence) {
+      paragraph = paragraph.concat(this.getRandomSentence());
+      firstSentence = false;
+    } else {
+      paragraph = paragraph.concat(" " + this.getRandomSentence());
+    }
+  }
+  return paragraph;
+}
+
+// Method to the GenerateNewText constructor function that gerates multiple paragraphs from paragraphs
+GenerateNewText.prototype.getAllParagraphs = function(numberOfParagraphs) {
+  let allParagraphs = [];
+  // Generate the number of paragraphs as specified by the user
+  while (allParagraphs.length < numberOfParagraphs) {
+    allParagraphs.push(this.getParagraph());
+  }
+  // Convert array into HTML string
+  let paragraphHTML = "";
+  allParagraphs.forEach(function (paragraph) {
+    paragraphHTML += "<p>" + paragraph + "</p>";
+  });
+  return paragraphHTML;
+}
+
+module.exports = loremIpsum;
